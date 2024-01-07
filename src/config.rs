af8d7c5c6a418182cef02c984cbf48dd899cfc62
phunguyen19 <phu.nguyen19@gmail.com>
2024-01-07 15:54:43 +0700

    CLI option fetch ticket status

--- src/config.rs
+++ src/config.rs
@@ -21,8 +21,10 @@ enum ProjectManagementName {
 
 impl Config {
     pub fn read_toml_file(path: &str) -> anyhow::Result<Config, ErrorKind> {
-        let toml = std::fs::read_to_string(path).map_err(|err| ErrorKind::ConfigError(ConfigErrorKind::IOError(err)))?;
-        let config: Config = toml::from_str(toml.as_str()).map_err(|err| ErrorKind::ConfigError(ConfigErrorKind::TOMLError(err)))?;
+        let toml = std::fs::read_to_string(path)
+            .map_err(|err| ErrorKind::ConfigError(ConfigErrorKind::IOError(err)))?;
+        let config: Config = toml::from_str(toml.as_str())
+            .map_err(|err| ErrorKind::ConfigError(ConfigErrorKind::TOMLError(err)))?;
         Ok(config)
     }
 }
@@ -31,19 +33,24 @@ impl Config {
 mod tests {
     use super::*;
 
-
     #[test]
     fn test_read_toml_file_not_exist() {
         let config = Config::read_toml_file("tests/fixtures/test-config.not-exist.toml");
         assert_eq!(config.is_err(), true);
-        assert!(config.unwrap_err().to_string().starts_with("Config error: IO error: No such file or directory"));
+        assert!(config
+            .unwrap_err()
+            .to_string()
+            .starts_with("Config error: IO error: No such file or directory"));
     }
 
     #[test]
     fn test_wrong_toml_format() {
         let config = Config::read_toml_file("tests/fixtures/test-config.wrong-format.toml");
         assert_eq!(config.is_err(), true);
-        assert!(config.unwrap_err().to_string().starts_with("Config error: TOML error: "));
+        assert!(config
+            .unwrap_err()
+            .to_string()
+            .starts_with("Config error: TOML error: "));
     }
 
     #[test]
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::process::Command;
 
 pub fn exit_with_error(error: &str) {
     eprintln!("{}", error);
@@ -48,6 +49,72 @@ pub fn get_jira_ticket_status(ticket_number: String) -> String {
             format!("Error: {}", status)
         }
     };
+}
+
+pub fn get_commits_info(branch: &str) -> anyhow::Result<Vec<String>> {
+    let command = format!("git log --format=%h%s --abbrev=7 {}", branch);
+    let output = Command::new("sh").arg("-c").arg(command).output()?;
+
+    if !output.status.success() {
+        exit_with_error(&format!(
+            "Fail to get commits info for branch '{}'. Error: {}",
+            branch,
+            String::from_utf8(output.stderr)?
+        ));
+    }
+
+    let commit_info = String::from_utf8(output.stdout)?;
+    let result = commit_info
+        .trim()
+        .split('\n')
+        .map(|commit| {
+            let (hash, message) = commit.split_at(7);
+            format!("{}::{}", hash, message)
+        })
+        .collect();
+
+    anyhow::Ok(result)
+}
+
+pub fn fetch_all() -> anyhow::Result<()> {
+    println!("Fetching all...");
+    let output = Command::new("git")
+        .arg("fetch")
+        .arg("--all")
+        .arg("--prune")
+        .arg("--tags")
+        .output()?;
+
+    if !output.status.success() {
+        let err = String::from_utf8(output.stderr)?;
+        exit_with_error(&format!("Fail to fetch all. Error: {}", err));
+    }
+
+    println!("{}", String::from_utf8(output.stdout)?);
+    anyhow::Ok(())
+}
+
+pub fn checkout_branch(branch: &str) -> anyhow::Result<()> {
+    let output = Command::new("git").arg("checkout").arg(branch).output()?;
+    if !output.status.success() {
+        let err = String::from_utf8(output.stderr)?;
+        exit_with_error(&format!(
+            "Fail to checkout branch '{}'. Error: {}",
+            branch, err
+        ));
+    }
+    println!("{}", String::from_utf8(output.stdout)?);
+    anyhow::Ok(())
+}
+
+pub fn pull_branch(branch: &str) -> anyhow::Result<()> {
+    let output = Command::new("git").arg("pull").output()?;
+    if !output.status.success() {
+        let err = String::from_utf8(output.stderr)?;
+        exit_with_error(&format!("Fail to pull branch '{}'. Error: {}", branch, err));
+    }
+    println!("{}", String::from_utf8(output.stdout)?);
+    anyhow::Ok(())
 }
 
 #[cfg(test)]
